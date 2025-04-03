67b11c2de455df03bf096b35e4c20321dff01404
Prasanna <sadhanala529@gmail.com>
2025-04-03 17:01:32 +0530

    first commit

---
+++ src/pages/Content/mockup/Utils/DeviceImages.js
@@ -0,0 +1,6 @@
+const URL = "chrome-extension://__MSG_@@extension_id__/assets";
+
+const PhoneTest = `${URL}/mockups/phone-test.png`;
+const wallpaper = `${URL}/mockups/1.png`;
+
+export { PhoneTest, wallpaper };
